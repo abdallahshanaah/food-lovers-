9b018b376eba1d80482830367ca1cb8b740fa3a6
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-02-05 13:36:33 +0200

    add js file and edit

---
+++ food.js
@@ -0,0 +1,7 @@
+ food + "!! that my favourite too ";
+   alert("Hello! are you food lovers");
+    var food = prompt("what is your favourite food ","fish"); 
+    
+        document.write = 
+            food+"!! that my favourite too" ; 
+            confirm( food+"!! that my favourite too" );
