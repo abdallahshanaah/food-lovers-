f2054e74b2f48d1bbb272f6292bfeb738248a315
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-02-06 09:25:46 +0200

    edit js file

--- food.js
+++ food.js
@@ -1,7 +1,32 @@
- food + "!! that my favourite too ";
-   alert("Hello! are you food lovers");
-    var food = prompt("what is your favourite food ","fish"); 
+alert("Hello! are you ready");
+var Name="";
+var Age="";
+var Major="";
+function information ()
+{
+    Name = prompt("what is your name ? ","abdallah shanaah"); 
+    Age = prompt("how old are you ? ",);
+    var again = confirm("DO YOU WANT TO ADD NEW PARSON ?");
+  console.log(again);
+    do {
+      
+
+    if(Age>=6 && Age<=12)
+    {console.log(Age);
+      return "name :"+Name+"\n"+"Age :"+Age+"\n"+"Major : Middle School";}
+    else if
+    (Age>=13 && Age<19){return "name :"+Name+"\n"+"Age :"+Age+"\n"+"Major : High School";}
+    else if
+    (Age>=19&& Age<=24){return "name :"+Name+"\n"+"Age :"+Age+"\n"+"Major : University";}
+    else if(Age==0){return "Error";}
+    else 
+    { return "name :"+Name+"\n"+"Age :"+Age+"\n"+"Major : YOU ARE EVEN BABY OR OLD TO STUDY";}
     
-        document.write = 
-            food+"!! that my favourite too" ; 
-            confirm( food+"!! that my favourite too" );
+  }while(again==true)
+  console.log(again);
+  
+}
+
+  document.write(information());
+            
+      
